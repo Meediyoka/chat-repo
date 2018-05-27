f095e4f736c317514c08382e531eae458139d7f3
Thomas <meediyoka@gmail.com>
2018-05-27 21:12:35 +1200

    Indent entirety of lambda_function
    Pure refraction of code indentation

--- lambda_cloud_function_code_skeleton.js
+++ lambda_cloud_function_code_skeleton.js
@@ -6,26 +6,26 @@ var docClient = new AWS.DynamoDB.DocumentClient({
 });
 
 exports.handler = function(event, context, callback) {
-    const paper = event.queryResult.parameters['Paper'];                     //The following variables store incoming parameter values from a 
-    const pre_req = event.queryResult.parameters['pre-req'];				// JSON file sent by DialogFlow. Depending on the users question or query
-    const co_req = event.queryResult.parameters['co-req'];				   // The parameters will be updated based on the context of request.
-    const key_date = event.queryResult.parameters['Keydates'];			   // E.G if a user asks about semester dates, the parameter keydates will be sent through.
+    const paper = event.queryResult.parameters['Paper']; //The following variables store incoming parameter values from a 
+    const pre_req = event.queryResult.parameters['pre-req']; // JSON file sent by DialogFlow. Depending on the users question or query
+    const co_req = event.queryResult.parameters['co-req']; // The parameters will be updated based on the context of request.
+    const key_date = event.queryResult.parameters['Keydates']; // E.G if a user asks about semester dates, the parameter keydates will be sent through.
     const manukau = event.queryResult.parameters['manukau'];
 
 
     function fallback(agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
-     }
+        agent.add(`I didn't understand`);
+        agent.add(`I'm sorry, can you try again?`);
+    }
 
 
-       // PRE-REQ HANDLER
-	   
-	   if (pre_req != null) {
+    // PRE-REQ HANDLER
+
+    if (pre_req != null) {
 
         var params = {
             TableName: 'paperInformationChatBot',
-            ProjectionExpression: 'Prerequisites, Paper_Name, PrerequisitesAdditional', 
+            ProjectionExpression: 'Prerequisites, Paper_Name, PrerequisitesAdditional',
             FilterExpression: 'Paper_Code = :paper_code',
             ExpressionAttributeValues: {
                 ":paper_code": paper
@@ -48,36 +48,33 @@ exports.handler = function(event, context, callback) {
                     callback(null, {
                         fulfillmentText: "" + paper_name + " has no prerequisites."
                     });
-                }
-                else if (paper_prerequisite != "None" && add_prerequisite == "None") {
+                } else if (paper_prerequisite != "None" && add_prerequisite == "None") {
                     callback(null, {
                         fulfillmentText: "The prerequisites for " + paper_name + " is " + paper_prerequisite
                     });
-                }
-                else if (paper_prerequisite == "None" && add_prerequisite != "None") {
+                } else if (paper_prerequisite == "None" && add_prerequisite != "None") {
                     callback(null, {
-                        fulfillmentText: "The prerequisites for " + paper_name + " are any one of " + add_prerequisite 
+                        fulfillmentText: "The prerequisites for " + paper_name + " are any one of " + add_prerequisite
                     });
-                }
-                else if (add_prerequisite != "None") {
+                } else if (add_prerequisite != "None") {
                     callback(null, {
                         fulfillmentText: "The prerequisite for " + paper_name + " is " + paper_prerequisite + " and any one of " + add_prerequisite
                     });
-                    
+
                 }
 
             }
 
         });
-        
-    
-        
-	   //CO-REQ HANDLER
-	   
-	   
+
+
+
+        //CO-REQ HANDLER
+
+
     } else if (co_req != null) {
-        
-            params = {
+
+        params = {
             TableName: 'paperInformationChatBot',
             ProjectionExpression: 'Corequisites, Paper_Name, PrerequisitesAdditional', // remove this string if you want to get not only 'name'
             FilterExpression: 'Paper_Code = :paper_code',
@@ -102,15 +99,13 @@ exports.handler = function(event, context, callback) {
                     callback(null, {
                         fulfillmentText: "" + paper_name + " has no corequisites."
                     });
-                }
-                else if(paper_corequisite != "None" && paper_prerequisite != "None"){
+                } else if (paper_corequisite != "None" && paper_prerequisite != "None") {
                     callback(null, {
                         fulfillmentText: "The corequisite for " + paper_name + " is " + paper_corequisite + " and the prerequisites are " + paper_prerequisite
                     });
-                }
-                else{
+                } else {
                     callback(null, {
-                        fulfillmentText: "The corequisite for " + paper_name + " is " +paper_corequisite
+                        fulfillmentText: "The corequisite for " + paper_name + " is " + paper_corequisite
                     });
                 }
 
@@ -118,125 +113,115 @@ exports.handler = function(event, context, callback) {
 
         });
 
-	   
-	   // KEY-DATE HANDLER
-                  }else if(key_date != null){
-           
-           var parameter2 = key_date[0];
-           var parameter1 = key_date[1];
-           
-           console.log(parameter1+parameter2);
-           
-           var table_scope;
-           var fulfillmentScopeText;
-           var fulfillmentScopeDate;
-           
-           var fulfillmentStart =  " starts ";
-           var fulfillmentEnd = " ends ";
-           var fulfillmentFirst = " has its first day ";
-           var fulfillmentLast = " has its last day ";
-           var fulfillmentBetween = " is available between ";
-           var semester1 = "Semester 1";
-           var semester2 = "Semester 2";
-           var summerSchool = "Summer School";
-           var easterBreak = "Easter break are ";
-           var midSemBreak = " mid semester break periods are "
-           
-           //SEMESTER 1 DATES
-           
-           if(parameter1 == "start" && parameter2 == "semester1"){
-               table_scope = "FirstDaySem1";
-               fulfillmentScopeText = fulfillmentFirst;
-               fulfillmentScopeDate = semester1;
-           }   
-           else if(parameter1 == "end" && parameter2 == "semester1"){
-               table_scope = "LastDaySem1";
-               fulfillmentScopeText = fulfillmentLast;
-               fulfillmentScopeDate = semester1;
-           }
-             else if(parameter1 == "semester1" && parameter2 == "when"){
-               table_scope = "Semester1Dates";
-               fulfillmentScopeText = fulfillmentBetween;
-               fulfillmentScopeDate = semester1;
-           }
-           
-           //SEMESTER 2 DATES
-           
-           else if(parameter1 == "start" && parameter2 == "semester2"){
-               table_scope = "FirstDaySem2";
-               fulfillmentScopeText = fulfillmentStart;
-               fulfillmentScopeDate = semester2;
-           }   
-           else if(parameter1 == "end" && parameter2 == "semester2"){
-               table_scope = "LastDaySem2";
-               fulfillmentScopeText = fulfillmentEnd;
-               fulfillmentScopeDate = semester2;
-           }
-          
-           else if(parameter1 == "semester2" && parameter2 == "when"){
-               table_scope = "Semester2Dates";
-               fulfillmentScopeText = fulfillmentBetween;
-               fulfillmentScopeDate = semester2;
-           }
-           
-          
-       // SUMMER SCHOOL DATES
-			else if(parameter1 == "end" && parameter2 == "summerschool"){
-               table_scope = "LastDaySummer";
-               fulfillmentScopeText = fulfillmentLast;
-               fulfillmentScopeDate = summerSchool;
-           }
-            else if(parameter1 == "start" && parameter2 == "summerschool"){
-               table_scope = "FirstDaySummer";
-               fulfillmentScopeText = fulfillmentStart;
-               fulfillmentScopeDate = summerSchool;
-           }
-            else if(parameter1 == "summerschool" && parameter2 == "when"){
-               table_scope = "SummerSchoolDates";
-               fulfillmentScopeText = fulfillmentBetween;
-               fulfillmentScopeDate = summerSchool;
-           }
-           
-       // EASTER BREAK
-           else if(parameter2 == "easter"){
-               table_scope = "EasterBreak";
-               fulfillmentScopeText = easterBreak;
-               fulfillmentScopeDate = "The dates for ";
-           }
-                  
-       // MID SEM BREAK
-           else if(parameter2 == "midsembreak" && parameter1 == "semester1"){
-               table_scope = "Sem1MidBreak";
-               fulfillmentScopeText = midSemBreak;
-               fulfillmentScopeDate = semester1;
 
-           }
-           else if(parameter2 == "midsembreak" && parameter1 == "semester2"){
-               table_scope = "Sem2MidBreak";
-               fulfillmentScopeText = midSemBreak;
-               fulfillmentScopeDate = semester2;
+        // KEY-DATE HANDLER
+    } else if (key_date != null) {
 
-           }
-           else if (parameter2 == "midsembreak"){
-               table_scope = "midSemBreak";
-               fulfillmentScopeText = midSemBreak;
-               fulfillmentScopeDate = "This years";
-           }
+        var parameter2 = key_date[0];
+        var parameter1 = key_date[1];
+
+        console.log(parameter1 + parameter2);
+
+        var table_scope;
+        var fulfillmentScopeText;
+        var fulfillmentScopeDate;
+
+        var fulfillmentStart = " starts ";
+        var fulfillmentEnd = " ends ";
+        var fulfillmentFirst = " has its first day ";
+        var fulfillmentLast = " has its last day ";
+        var fulfillmentBetween = " is available between ";
+        var semester1 = "Semester 1";
+        var semester2 = "Semester 2";
+        var summerSchool = "Summer School";
+        var easterBreak = "Easter break are ";
+        var midSemBreak = " mid semester break periods are "
+
+        //SEMESTER 1 DATES
+
+        if (parameter1 == "start" && parameter2 == "semester1") {
+            table_scope = "FirstDaySem1";
+            fulfillmentScopeText = fulfillmentFirst;
+            fulfillmentScopeDate = semester1;
+        } else if (parameter1 == "end" && parameter2 == "semester1") {
+            table_scope = "LastDaySem1";
+            fulfillmentScopeText = fulfillmentLast;
+            fulfillmentScopeDate = semester1;
+        } else if (parameter1 == "semester1" && parameter2 == "when") {
+            table_scope = "Semester1Dates";
+            fulfillmentScopeText = fulfillmentBetween;
+            fulfillmentScopeDate = semester1;
+        }
+
+        //SEMESTER 2 DATES
+        else if (parameter1 == "start" && parameter2 == "semester2") {
+            table_scope = "FirstDaySem2";
+            fulfillmentScopeText = fulfillmentStart;
+            fulfillmentScopeDate = semester2;
+        } else if (parameter1 == "end" && parameter2 == "semester2") {
+            table_scope = "LastDaySem2";
+            fulfillmentScopeText = fulfillmentEnd;
+            fulfillmentScopeDate = semester2;
+        } else if (parameter1 == "semester2" && parameter2 == "when") {
+            table_scope = "Semester2Dates";
+            fulfillmentScopeText = fulfillmentBetween;
+            fulfillmentScopeDate = semester2;
+        }
+
+
+        // SUMMER SCHOOL DATES
+        else if (parameter1 == "end" && parameter2 == "summerschool") {
+            table_scope = "LastDaySummer";
+            fulfillmentScopeText = fulfillmentLast;
+            fulfillmentScopeDate = summerSchool;
+        } else if (parameter1 == "start" && parameter2 == "summerschool") {
+            table_scope = "FirstDaySummer";
+            fulfillmentScopeText = fulfillmentStart;
+            fulfillmentScopeDate = summerSchool;
+        } else if (parameter1 == "summerschool" && parameter2 == "when") {
+            table_scope = "SummerSchoolDates";
+            fulfillmentScopeText = fulfillmentBetween;
+            fulfillmentScopeDate = summerSchool;
+        }
+
+        // EASTER BREAK
+        else if (parameter2 == "easter") {
+            table_scope = "EasterBreak";
+            fulfillmentScopeText = easterBreak;
+            fulfillmentScopeDate = "The dates for ";
+        }
+
+        // MID SEM BREAK
+        else if (parameter2 == "midsembreak" && parameter1 == "semester1") {
+            table_scope = "Sem1MidBreak";
+            fulfillmentScopeText = midSemBreak;
+            fulfillmentScopeDate = semester1;
+
+        } else if (parameter2 == "midsembreak" && parameter1 == "semester2") {
+            table_scope = "Sem2MidBreak";
+            fulfillmentScopeText = midSemBreak;
+            fulfillmentScopeDate = semester2;
+
+        } else if (parameter2 == "midsembreak") {
+            table_scope = "midSemBreak";
+            fulfillmentScopeText = midSemBreak;
+            fulfillmentScopeDate = "This years";
+        }
 
 
 
 
-           console.log(table_scope);
+        console.log(table_scope);
 
-           params = {
+        params = {
             TableName: 'generalInformationChatbot',
             ProjectionExpression: 'dateValue', // remove this string if you want to get not only 'name'
             FilterExpression: 'informationType = :table_scope',
             ExpressionAttributeValues: {
                 ":table_scope": table_scope
             }
-           };
-           docClient.scan(params, function(err, data) {
+        };
+        docClient.scan(params, function(err, data) {
             if (err) {
                 console.log(err, null); // an error occurred // 
             } else {
@@ -247,22 +232,22 @@ exports.handler = function(event, context, callback) {
                 console.log(dateValue);
 
 
-            callback(null, {
-                        fulfillmentText: fulfillmentScopeDate + fulfillmentScopeText + dateValue
-                    });
+                callback(null, {
+                    fulfillmentText: fulfillmentScopeDate + fulfillmentScopeText + dateValue
+                });
 
 
             }
 
         })
-                  
-       // MANUKAU
-	   
-	   }else if(manukau != null){
+
+        // MANUKAU
+
+    } else if (manukau != null) {
         console.log(paper);
-         var params = {
+        var params = {
             TableName: 'paperInformationChatBot',
-            ProjectionExpression: 'Paper_Name, Manukau', 
+            ProjectionExpression: 'Paper_Name, Manukau',
             FilterExpression: 'Paper_Code = :paper_code',
             ExpressionAttributeValues: {
                 ":paper_code": paper
@@ -278,46 +263,42 @@ exports.handler = function(event, context, callback) {
 
                 var paper_name = data.Items[0].Paper_Name;
                 var is_manukau = data.Items[0].Manukau;
-          
-          if(is_manukau == "Yes, Semester 2 has availability"){
-             callback(null, {
+
+                if (is_manukau == "Yes, Semester 2 has availability") {
+                    callback(null, {
                         fulfillmentText: is_manukau + " for " + paper_name
                     });
-                
 
-            }
-           else if(is_manukau == "Yes, Semester 1 has availability"){
-             callback(null, {
+
+                } else if (is_manukau == "Yes, Semester 1 has availability") {
+                    callback(null, {
                         fulfillmentText: is_manukau + " for " + paper_name
                     });
-                
 
-            }
-            else if(is_manukau == "No"){
-             callback(null, {
+
+                } else if (is_manukau == "No") {
+                    callback(null, {
                         fulfillmentText: "There are unfortunately no streams for " + paper_name + " at Manukau Campus this year"
                     });
-                
 
-            }
-            else if(is_manukau == "Yes, Both Semesters are available"){
-             callback(null, {
+
+                } else if (is_manukau == "Yes, Both Semesters are available") {
+                    callback(null, {
                         fulfillmentText: is_manukau + " for " + paper_name
                     });
-                
 
-            }
-            else{
-                callback(null, {
+
+                } else {
+                    callback(null, {
                         fulfillmentText: "Sorry, I failed to understand your question regarding Manukau"
                     });
+                }
             }
-        }
         })
-        
-        
-     
-        
+
+
+
+
     } else {
         callback(null, {
             fulfillmentText: "Nothing was called!"
